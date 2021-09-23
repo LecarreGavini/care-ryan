f59809e760ee08c9b4f340c6403a27ba63a7dde7
Lecarre Gavini <lecarre@lanexus.com>
2021-09-23 20:34:45 +0800

    chore: format layout

--- components/layout.js
+++ components/layout.js
@@ -5,67 +5,65 @@ import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
 
 const name = 'Your Name'
-export const siteTitle = 'Next.js Sample Websites'
+export const siteTitle = 'Cafe Ryan - Food and Drinks'
 
 export default function Layout({ children, home }) {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/images/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content='./favicon.ico'
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/coffee.jpeg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/coffee.jpeg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
-              <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
-              </Link>
-            </h2>
-          </>
-        )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-    </div>
-  )
+	return (
+		<div className={styles.container}>
+			<Head>
+				<link rel='icon' href='/images/favicon.ico' />
+				<meta
+					name='description'
+					content='Learn how to build a personal website using Next.js'
+				/>
+				<meta property='og:image' content='./favicon.ico' />
+				<meta name='og:title' content={siteTitle} />
+				<meta name='twitter:card' content='summary_large_image' />
+			</Head>
+			<header></header>
+			<header className={styles.header}>
+				{home ? (
+					<>
+						<Image
+							priority
+							src='/images/coffee.jpeg'
+							className={utilStyles.borderCircle}
+							height={144}
+							width={144}
+							alt={name}
+						/>
+						<h1 className={utilStyles.heading2Xl}>{name}</h1>
+					</>
+				) : (
+					<>
+						<Link href='/'>
+							<a>
+								<Image
+									priority
+									src='/images/coffee.jpeg'
+									className={utilStyles.borderCircle}
+									height={108}
+									width={108}
+									alt={name}
+								/>
+							</a>
+						</Link>
+						<h2 className={utilStyles.headingLg}>
+							<Link href='/'>
+								<a className={utilStyles.colorInherit}>{name}</a>
+							</Link>
+						</h2>
+					</>
+				)}
+			</header>
+			<main>{children}</main>
+			{!home && (
+				<div className={styles.backToHome}>
+					<Link href='/'>
+						<a>← Back to home</a>
+					</Link>
+				</div>
+			)}
+		</div>
+	)
 }
