1ebf7b614ca2c21841a6852ffb01f0890de7f5e6
Lecarre Gavini <lecarre@lanexus.com>
2021-09-23 20:17:57 +0800

    chore: update page

--- components/layout.js
+++ components/layout.js
@@ -5,22 +5,20 @@ import utilStyles from '../styles/utils.module.scss'
 import Link from 'next/link'
 
 const name = 'Your Name'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'Next.js Sample Websites'
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content='./favicon.ico'
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
@@ -30,7 +28,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="/images/coffee.jpeg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -44,7 +42,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src="/images/profile.jpg"
+                  src="/images/coffee.jpeg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
